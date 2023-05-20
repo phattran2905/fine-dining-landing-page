6d76a26319011f6b3d3d6492d928af11dd08bea9
phat@minim1 <phat.tranducvn@gmail.com>
2023-05-19 20:23:06 -0700

    Handle slide between gallery images

--- src/component/GalleryItem.tsx
+++ src/component/GalleryItem.tsx
@@ -7,20 +7,16 @@ type Props = {
 
 function GalleryItem({ imgSrc, title }: Props) {
   return (
-    <div className="relative my-4 cursor-pointer hover:blur-[1px]">
-      <div className=" h-[447px] md:my-0 md:w-[301px]">
-        <img
-          src={imgSrc}
-          alt={title}
-          className="h-[100%] w-[100%] object-cover "
-        />
-      </div>
-
-      <div className="absolute inset-0 h-full w-full items-stretch justify-center hover:flex hidden">
-        <div className="opacity-1 flex h-full items-center justify-center">
-          <AiOutlineCamera size={30} className="text-white" />
-        </div>
-      </div>
+    <div className="group/card relative flex h-[447px] min-w-[301px] cursor-pointer items-center justify-center mx-1 ">
+      <img
+        src={imgSrc}
+        alt={title}
+        className="h-[100%] w-[100%] object-cover opacity-100 group-hover/card:opacity-30"
+      />
+      <AiOutlineCamera
+        size={30}
+        className="absolute cursor-pointer text-white opacity-0 group-hover/card:opacity-100"
+      />
     </div>
   );
 }
--- src/scenes/PhotoGallery.tsx
+++ src/scenes/PhotoGallery.tsx
@@ -3,15 +3,29 @@ import styles from "../styles";
 import Button from "../component/Button";
 import SubHeading from "../component/SubHeading";
 import GalleryItem from "../component/GalleryItem";
+import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
+import { useRef } from "react";
 
 type Props = {};
 function PhotoGallery({}: Props) {
+  const scrollRef = useRef(null);
+
+  const scroll = (direction: string) => {
+    const { current } = scrollRef;
+
+    if (direction === "left") {
+      (current as any).scrollLeft -= 300;
+    } else {
+      (current as any).scrollLeft += 300;
+    }
+  };
+
   return (
     <section
       id="gallery"
       className="flex flex-col items-center justify-between p-10 md:ml-20 md:flex-row md:p-0 md:py-10"
     >
-      <div className="mb-10 flex flex-col items-start justify-center md:mb-0 md:basis-2/5">
+      <div className="mb-10 mr-10 flex basis-1/2 flex-col items-start justify-center md:mb-0 md:basis-2/5">
         <SubHeading title="Instagram" />
 
         <h2 className={`${styles.heading2}`}>Photo Gallery</h2>
@@ -24,11 +38,26 @@ function PhotoGallery({}: Props) {
         <Button>View More</Button>
       </div>
 
-      <div className="flex w-full flex-col gap-x-4 md:basis-3/5 md:flex-row md:overflow-x-scroll">
-        <GalleryItem imgSrc={images.gallery01} title={"gallery01"} />
-        <GalleryItem imgSrc={images.gallery02} title={"gallery02"} />
-        <GalleryItem imgSrc={images.gallery03} title={"gallery03"} />
-        <GalleryItem imgSrc={images.gallery04} title={"gallery04"} />
+      <div className="relative flex w-full basis-1/2 scroll-m-0 items-center justify-center gap-x-4 p-10 md:basis-3/5 md:overflow-x-hidden md:p-0">
+        <div className="group/container flex overflow-x-scroll" ref={scrollRef}>
+          <GalleryItem imgSrc={images.gallery01} title={"gallery01"} />
+          <GalleryItem imgSrc={images.gallery02} title={"gallery02"} />
+          <GalleryItem imgSrc={images.gallery03} title={"gallery03"} />
+          <GalleryItem imgSrc={images.gallery04} title={"gallery04"} />
+        </div>
+
+        <div className="absolute flex w-full items-center justify-between">
+          <BsArrowLeftShort
+            className="cursor-pointer bg-gray text-white hover:bg-crimson hover:text-black"
+            size={50}
+            onClick={() => scroll("left")}
+          />
+          <BsArrowRightShort
+            className="cursor-pointer  bg-gray text-white hover:bg-crimson hover:text-black"
+            size={50}
+            onClick={() => scroll("right")}
+          />
+        </div>
       </div>
     </section>
   );
